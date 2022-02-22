df80ea122c6d93cb700ba373094d0cd5ec160ab7
Lihuel <lihuel.almada@terragene.com>
2022-02-22 13:20:51 -0300

    add detail pokemon request

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -5,13 +5,15 @@ export async function getPokemonsApi() {
     const url = `${API_HOST}/pokemon?limit=20&offset=0`;
     const response = await fetch(url);
     const result = await response.json();
+   
     return result
+
   } catch (error) {
     throw error;
   }
 }
 
-export async function getPokemonDetailsByUrlApi() {
+export async function getPokemonDetailsByUrlApi(url) {
 
   try {
     const response = await fetch(url);
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -3,9 +3,8 @@ import { View, Text } from "react-native";
 import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function PokedexScreen() {
-
-	const [pokemons, setPokemons] = useState([])
-	console.log("-------------- Pokemong ----------", pokemons)
+  const [pokemons, setPokemons] = useState([]);
+  console.log("-------------- Pokemong ----------", pokemons);
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -15,25 +14,24 @@ export default function PokedexScreen() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsApi();
-
+      //console.log(response)
       const pokemonsArray = [];
-			for await (pokemon of response.result) {
-				
-				const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
-				
-				pokemonsArray.push({
-					id: pokemonDetails.id,
-					name: pokemonDetails.name,
-					type: pokemonDetails.types[0].type.name,
-					order: pokemonDetails.order,
-					//imagen: pokemonDetails.sprites.other['generation-v'].black_wite.animated.front_default
-					imagen: pokemonDetails.sprites.other['official-artwork'].front_default
-				})
-				setPokemons([...pokemons, ...pokemonsArray]);
-			}
-      //console.log(response);
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        console.log('pokemonDetails', pokemonDetails);
+
+        pokemonsArray.push({
+        	id: pokemonDetails.id,
+        	name: pokemonDetails.name,
+        	type: pokemonDetails.types[0].type.name,
+        	order: pokemonDetails.order,
+        	//imagen: pokemonDetails.sprites.other['generation-v'].black_wite.animated.front_default
+        	imagen: pokemonDetails.sprites.other['official-artwork'].front_default
+        })
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
-      console.log("error", error);
+      console.error(error);
     }
   };
 
