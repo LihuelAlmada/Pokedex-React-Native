3f3d54d66ac1791369188cb11b59a1ac6522d316
Necralma <lihuel.almada@gmail.com>
2022-03-11 11:27:54 -0300

    add request to api pokemon of id

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -15,13 +15,22 @@ export async function getPokemonsApi(endpointUrl) {
 }
 
 export async function getPokemonDetailsByUrlApi(url) {
-
   try {
     const response = await fetch(url);
     const result = await response.json();
     return result;
   } catch (error) {
     throw error;
-    
+  }
+}
+
+export async function getPokemonDetailsApi(od) {
+  try {
+    const url = `${API_HOST}/pokemon/${id}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
   }
 }
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,10 +1,26 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {getPokemonDetailsApi} from '../api/pokemon'
 
-const PokemonScreen = () => {
+const PokemonScreen = (props) => {
+  const {
+    navigation, route: {params}
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
+  useEffect(()=>{
+    (async () => {
+      try {
+        const response = await get
+      } catch(error) {
+        navigation.goBack();
+      }
+    })()
+  })
+
+  if (!pokemon) return null;
   return (
     <View>
-      <Text>Esto es un PokemonScreen</Text>
+      <Text>Esto es un {pokemon}</Text>
     </View>
   );
 };
