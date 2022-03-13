4df538fd62621349869dd617150bc4df3faf34df
Necralma <lihuel.almada@gmail.com>
2022-03-12 22:43:35 -0300

    finish pokemon detail

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -2,12 +2,23 @@ import { View, Text, ScrollView } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {getPokemonDetailsApi} from '../api/pokemon'
 import Header from '../components/Pokemon/Header';
-
+import Types from '../components/Pokemon/Types'
+import Stats from '../components/Pokemon/Stats'
+import {
+  AntDesign,
+  FontAwesome
+} from "@expo/vector-icons";
 const PokemonScreen = (props) => {
   const {
     navigation, route: {params}
   } = props;
   const [pokemon, setPokemon] = useState(null);
+  useEffect(()=>{
+     navigation.setOptions({
+       headerRight: () => <FontAwesome name="heart" size={24} color='white' />,
+       headerLeft: () => <AntDesign name="arrowleft" size={24} color="white" onPress={navigation.goBack}/>
+     })
+  }, [navigation, params])
   useEffect(()=>{
     (async () => {
       try {
@@ -24,8 +35,8 @@ const PokemonScreen = (props) => {
   return (
     <ScrollView>
       <Header name={pokemon.name} order={pokemon.order} image={pokemon.sprites.other['official-artwork'].front_default} type={pokemon.types[0].type.name}/>
-      <Text>Esto es un {pokemon.id}</Text>
-      <Text>{pokemon.name}</Text>
+      <Types types={pokemon.types}/>
+      <Stats stats={pokemon.stats}/>
     </ScrollView>
   );
 };
