6ec096b61fccdb4c5ca7a3e240cb335f0f342e0b
Necralma <lihuel.almada@gmail.com>
2022-03-13 18:18:30 -0300

    add formik and yup validation

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,37 +1,78 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 export default function LoginForm() {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValues) => {
+      console.log('formulario enviado');
+      console.log( formValues);
+    }
+  });
   return (
     <View>
       <Text style={styles.title}>Iniciar Sesión</Text>
-      <TextInput placeholder="Usuario"
+      <TextInput
+        placeholder="Usuario"
         style={styles.input}
-        autoCapitalize='none'
+        autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text)=> formik.setFieldValue('username', text)}
       />
-      <TextInput placeholder="Contraseña"
+      <Text style={styles.error}>{formik.errors.username}</Text>
+
+      <TextInput
+        placeholder="Contraseña"
         style={styles.input}
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text)=> formik.setFieldValue('password', text)}
+
       />
-      <Button title="Enviar" onPress={() => console.log('Enviar')} />
+      <Text style={styles.error}>{formik.errors.password}</Text>
+
+      <Button title="Enviar" onPress={formik.handleSubmit} />
+
     </View>
-  )
+  );
 }
 
+
+function initialValues () {
+  return{
+    username: '',
+    password: '',
+  }
+}
+function validationSchema() {
+  return {
+    username: yup.string().required("El usuario es obligatorio"),
+    password: yup.string().required("El Pass es obligatorio"),
+  }
+}
 const styles = StyleSheet.create({
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 28,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 50,
-    marginBottom: 20
+    marginBottom: 20,
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20
   }
-
-})
+});
